31f689df51df1069c02e77845aab8be67549f107
Hannah Helms <helmshannahpatrice@gmail.com>
2017-11-05 14:02:24 -0500

    Test out problem 5 with no luck
    Moving on in HW as I wait for response from Prof. Yockey.

--- js/main.js
+++ js/main.js
@@ -1,7 +1,13 @@
 var clickSubmit = document.getElementById('submit');
+var firstName = document.getElementById('firstName');
+var lastName = document.getElementById('lastName');
+var emailAddress = document.getElementById('emailAddress');
+var messageText = document.getElementById('messageText');
 
-window.onbeforeunload = function () {
-    clickSubmit.addEventListener('submit', function () {
-        console.log('The firstName is: ' document.getElementsByName('firstname'))
-    });
-}
+clickSubmit.addEventListener('submit', function () {
+    event.preventDefault();
+    console.log('The firstName is:' + firstName.inputType);
+    console.log('The lastName is:' + lastName.value);
+    console.log('The email is:' + emailAddress.value);
+    console.log('The message is:' + messageText.value);
+});
