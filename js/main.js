695c1c187af1589d852dc78927b7a64cc022d019
Hannah Helms <helmshannahpatrice@gmail.com>
2017-11-05 15:55:07 -0500

    Add JS event listers to elements on page

--- js/main.js
+++ js/main.js
@@ -4,10 +4,36 @@
 //    document.changeImage.src = 'img/prezfuchs.jpg';
 //});
 
-var headingSubtitle = document.getElementById('heading');
+// WITH MOUSEOVER ADD FIGCAPTION
 
-headingSubtitle.addEventListener('click', function () {
-    var subtitle = document.createElement('h2');
-    subtitle.innerHTML = 'President of the University of Florida';
-    document.getElementById('addSubtitleHere')[0].appendChild(subtitle);
+var captionThis = document.getElementById('Fuchs');
+
+captionThis.addEventListener('mouseover', function () {
+    var caption = document.createElement('figcaption');
+    caption.innerHTML = 'Can I get a "Go Gators"?';
+    document.getElementsByClassName('addText')[0].appendChild(caption);
+});
+
+// WITH CLICK REMOVE IMG
+
+var logoUF = document.getElementById('logoUF');
+
+logoUF.addEventListener('click', function () {
+    var imgRemove = document.getElementById('logoUF');
+    imgRemove.remove();
+    var caption = document.createElement('figcaption');
+    caption.innerHTML = 'Where did it go?';
+    document.getElementsByClassName('whereGo')[0].appendChild(caption);
+});
+
+// WITH DOUBLECLICK ADD IMG
+
+var asideStuff = document.getElementById('addImg');
+
+asideStuff.addEventListener('dblclick', function () {
+    var itsGreat = document.createElement('img');
+    itsGreat.src = './img/gators.png';
+    itsGreat.setAttribute('width', '100%');
+    itsGreat.setAttribute('alt', 'Gators');
+    document.getElementsByClassName('asideStuff')[0].appendChild(itsGreat);
 });
