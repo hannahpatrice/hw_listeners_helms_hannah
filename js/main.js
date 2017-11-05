ae46ebab2d70a1467073a57468828ff1892bcd7a
Hannah Helms <helmshannahpatrice@gmail.com>
2017-11-04 21:22:23 -0400

    Input Prof Yockey's suggestions
    X still not incrementing.

--- js/main.js
+++ js/main.js
@@ -3,7 +3,6 @@ var headingText = document.getElementById('clickMe');
 headingText.addEventListener('click', function () {
     var paragraph = document.createElement('p');
     var numberX = 1;
-    paragraph.innerHTML = 'This is click number ' + numberX;
-    numberX++;
+    paragraph.innerHTML = 'This is click number ' + numberX++;
     document.getElementsByClassName('container')[0].appendChild(paragraph);
 });
