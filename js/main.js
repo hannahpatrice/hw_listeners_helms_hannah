05a520a1dcc28e7dfc64b59f79f27b82c0fa4547
Hannah Helms <helmshannahpatrice@gmail.com>
2017-11-04 22:23:56 -0400

    Move numberX variable to fix scope issue

--- js/main.js
+++ js/main.js
@@ -1,8 +1,8 @@
 var headingText = document.getElementById('clickMe');
+var numberX = 1;
 
 headingText.addEventListener('click', function () {
     var paragraph = document.createElement('p');
-    var numberX = 1;
     paragraph.innerHTML = 'This is click number ' + numberX++;
     document.getElementsByClassName('container')[0].appendChild(paragraph);
 });
