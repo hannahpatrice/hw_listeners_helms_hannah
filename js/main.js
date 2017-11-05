eb88d0443d0f7483908d620cce5e2eb6eb4f7f2c
Hannah Helms <helmshannahpatrice@gmail.com>
2017-11-05 16:08:27 -0500

    Set variable to form to release event listener

--- js/main.js
+++ js/main.js
@@ -1,13 +1,13 @@
-var clickSubmit = document.getElementById('submit');
-var firstName = document.getElementById('firstName');
-var lastName = document.getElementById('lastName');
-var emailAddress = document.getElementById('emailAddress');
-var messageText = document.getElementById('messageText');
+var clickSubmit = document.getElementById('iAmAForm');
 
 clickSubmit.addEventListener('submit', function () {
     event.preventDefault();
-    console.log('The firstName is:' + firstName.inputType);
-    console.log('The lastName is:' + lastName.value);
-    console.log('The email is:' + emailAddress.value);
-    console.log('The message is:' + messageText.value);
+    var firstName = document.getElementById('firstName');
+    var lastName = document.getElementById('lastName');
+    var emailAddress = document.getElementById('emailAddress');
+    var messageText = document.getElementById('messageText');
+    console.log('The firstName is: ' + firstName.value);
+    console.log('The lastName is: ' + lastName.value);
+    console.log('The email is: ' + emailAddress.value);
+    console.log('The message is: ' + messageText.value);
 });
